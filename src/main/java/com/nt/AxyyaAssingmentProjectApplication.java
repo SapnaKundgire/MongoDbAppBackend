f8f5b93455556bb653b6652f04875e19986f81d0
SapnaKundgire <sapnakundgire@gmail.com>
2022-07-04 15:15:34 +0530

    adding yaml mainifest

--- src/main/java/com/nt/AxyyaAssingmentProjectApplication.java
+++ src/main/java/com/nt/AxyyaAssingmentProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication 
-
+@EnableSwagger2
 public class AxyyaAssingmentProjectApplication {
 
 	public static void main(String[] args) {
